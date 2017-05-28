38803a0038718e974fcf346f34af9a339d5f02f5
Dave <drregg6@gmail.com>
2017-05-27 20:35:40 -0400

    changed the js around so desktop classes work on window resize

--- airbnb_footer/assets/js/scripts.js
+++ airbnb_footer/assets/js/scripts.js
@@ -1,15 +1,38 @@
+// grab universal elements and variables
 var screenWidth = window.innerWidth;
 var desktopDivs = document.querySelectorAll('.desktop-elements');
 var formContainer = document.querySelector('.form-container');
 
-// temporary fix for hiding site map
 
-if (screenWidth > 992) {
-    formContainer.classList.add('col-lg-3');
-//    for (var i = 0; i < desktopDivs.length; i++) {
-//        desktopDivs[i].classList.remove('hide');
-//    }
-    desktopDivs.forEach(function(div, i) {
-        div.classList.remove('hide');
-    })
+// add event listeners to window
+window.onload = desktopCssRules;
+window.onresize = getScreenWidth;
+
+
+// add css classes to desktop elements when screen size is appropriate
+function desktopCssRules() {
+    if (screenWidth > 992) {
+        formContainer.classList.add('col-lg-3');
+        // remove the .hide class from all site map elements
+        desktopDivs.forEach(function(div, i) {
+            div.classList.remove('hide');
+        });
+    } else {
+        // if it's anything BUT the desktop, but the classes exist, remove them
+        if (formContainer.classList.contains('col-lg-3')) {
+            formContainer.classList.remove('col-lg-3');
+        }
+        if (!desktopDivs[0].classList.contains('hide')) {
+            desktopDivs.forEach(function(div, i) {
+                div.classList.add('hide');
+            });
+        }
+    }
+}
+
+// get the screenWidth and apply the desktopCssRules on resize
+function getScreenWidth() {
+    screenWidth = window.innerWidth;
+    
+    desktopCssRules();
 }
