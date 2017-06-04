a78961a665a890aa183bab9fc3e156c6c6e75978
Dave <drregg6@gmail.com>
2017-06-04 19:49:38 -0400

    everything's easier with jQuery - commented out vanilla js

--- pp_nav/assets/js/scripts.js
+++ pp_nav/assets/js/scripts.js
@@ -1,3 +1,34 @@
+$(document).ready(function() {
+    $('#business').on('click', function() {
+        $('.dd-business').slideToggle( "slow" );
+    });
+    $('#personal').on('click', function() {
+        $('.dd-personal').slideToggle( "normal" );
+    });
+    $('.dd-close').on('click', function() {
+        console.log(this.parentElement);
+        $(this).parent().slideUp( "normal" );
+    });
+})
+
+
+
+
+/*
+personal and business
+dropdown slowly reveals <--- add (jquery or css)
+image pushed down <--- add (jquery or css)
+else
+.dropdown hides
+*/
+/*
+if dropdown is open
+window onclick
+if !.dropdown is clicked
+close dropdown
+*/
+// close when the window is clicked anywhere BUT the element
+
 // grab needed elements
 var menuItemPersonal = document.getElementById('personal');
 var menuItemBusiness = document.getElementById('business');
@@ -6,80 +37,59 @@ var dropdownBusiness = document.querySelector('.dd-business');
 var closeElements = document.querySelectorAll('.dd-close');
 
 // add eventlisteners to elements
-// testers
-menuItemBusiness.addEventListener('click', business);
-menuItemPersonal.addEventListener('click', personal);
-closeElements.forEach(function(closeElement, i) {
-    closeElement.addEventListener('click', close);
-})
+//menuItemBusiness.addEventListener('click', business);
+//menuItemPersonal.addEventListener('click', personal);
+//closeElements.forEach(function(closeElement, i) {
+//    closeElement.addEventListener('click', close);
+//})
 
+// testers
 //window.onclick = windowTest;
 //
 //if (!dropdownPersonal.classList.contains('dropdown') || !dropdownBusiness.classList.contains('dropdown')) {
 //    window.onclick = close;
 //}
 
-/*
-personal and business
-onclick
-if .dropdown is not visible
-dropdown slowly reveals
-image pushed down
-arrow will animate upwards
-else
-.dropdown hides
-*/
-/*
-x button on
-onclick
-dropdown slowly closes
-arrow will animate downwards
-*/
-/*
-if dropdown is open
-window onclick
-if !.dropdown is clicked
-close dropdown
-*/
-
 
 // functions
-function close() {
-    menuItemPersonal.classList.remove('li-selected');
-    menuItemBusiness.classList.remove('li-selected');
-    dropdownBusiness.classList.add('dropdown');
-    dropdownPersonal.classList.add('dropdown');
-}
-
-function personal() {
-    if (!dropdownPersonal.classList.contains('dropdown')) {
-        close();
-    } else if (!dropdownBusiness.classList.contains('dropdown')) {
-        close();
-        menuItemPersonal.classList.add('li-selected');
-        dropdownPersonal.classList.remove('dropdown');
-    } else {
-        menuItemPersonal.classList.add('li-selected');
-        dropdownPersonal.classList.remove('dropdown');
-    }
-}
-
-function business() {
-    if (!dropdownBusiness.classList.contains('dropdown')) {
-        close();
-    } else if (!dropdownPersonal.classList.contains('dropdown')) {
-        close();
-        menuItemBusiness.classList.add('li-selected');
-        dropdownBusiness.classList.remove('dropdown');
-    } else {
-        menuItemBusiness.classList.add('li-selected');
-        dropdownBusiness.classList.remove('dropdown');
-    }
-}
+//function close() {
+//    menuItemPersonal.classList.remove('li-selected');
+//    menuItemBusiness.classList.remove('li-selected');
+//    dropdownBusiness.classList.add('dropdown');
+//    dropdownPersonal.classList.add('dropdown');
+//}
+//
+//function personal() {
+//    if (!dropdownPersonal.classList.contains('dropdown')) {
+//        close();
+//    } else if (!dropdownBusiness.classList.contains('dropdown')) {
+//        close();
+//        menuItemPersonal.classList.add('li-selected');
+//        dropdownPersonal.classList.remove('dropdown');
+//    } else {
+//        menuItemPersonal.classList.add('li-selected');
+//        dropdownPersonal.classList.remove('dropdown');
+//    }
+//}
+//
+//function business() {
+//    if (!dropdownBusiness.classList.contains('dropdown')) {
+//        close();
+//    } else if (!dropdownPersonal.classList.contains('dropdown')) {
+//        close();
+//        menuItemBusiness.classList.add('li-selected');
+//        dropdownBusiness.classList.remove('dropdown');
+//    } else {
+//        menuItemBusiness.classList.add('li-selected');
+//        dropdownBusiness.classList.remove('dropdown');
+//    }
+//}
 
 //function windowTest() {
-//    console.log("I work!")
 //    if (!dropdownPersonal.classList.contains('dropdown') || !dropdownBusiness.classList.contains('dropdown')) {
+//        console.log("I work!")
 //        close();
+//    } else {
+//        console.log("Meh");
 //    }
 //}
